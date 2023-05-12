9e73f53785229745d90f1a4dcbb6e44c46fee833
NelsonAMR <nelson.macias99@gmail.com>
2023-05-12 10:41:26 -0600

    Creacion de componente details, falta agregar logica

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { Fragment, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Products, Slider } from "../../components";
 
-import heroSliderData from "../../utils/data-slider";
+import { heroSliderData } from "../../utils";
 import { getProducts } from "../../redux/actions/productActions.";
 
 function Home() {
