04463d3b65f7e73587fca3fa856b1810036136dc
michelle margan <michimargan@hotmail.com>
2023-05-12 17:23:15 -0300

    vista del form hecha

--- src/pages/CreateUser/CreateUser.tsx
+++ src/pages/CreateUser/CreateUser.tsx
@@ -1,5 +1,37 @@
-//import styles from "./CreateUser.module.scss"
+import styles from "./CreateUser.module.scss"
 
 export default function (){
-    return <div>form</div>
+    return( 
+    <div className={styles.contenedor}>
+        <div className={styles.contenedor}>
+            <button className={styles.buttonAtras}>Atrás</button>
+            <h2 className={styles.title}>Registrate</h2>
+            <form className={styles.contForm}>
+                <div className={styles.contForm}>
+                    <div className={styles.form}>
+                        <label htmlFor="Usuario">Usuario</label>
+                        <input type="text" name="usuario" className={styles.input}/>
+
+                        <label htmlFor="Contraseña" className={styles.name}>Contraseña</label>
+                        <input type="password" name="contraseña" className={styles.input}/>
+
+                        <label htmlFor="Repita contraseña">Repita contraseña</label>
+                        <input type="password" name="repita contraseña" className={styles.input}/>
+                    </div>
+                    <div className={styles.form}>
+                        <label htmlFor="Correo">Correo</label>
+                        <input type="email" name="correo" className={styles.input}/>
+
+                        <label htmlFor="Telefono">Telefono</label>
+                        <input type="number" name="telefono" className={styles.input}/>
+
+                        <label htmlFor="Fecha de nacimiento">Fecha de nacimiento</label>
+                        <input type="date" name="fecha de nacimiento" className={styles.input}/>
+                    </div>
+                </div>
+                <button className={styles.buttonRes}>Registrar</button>
+            </form>
+        </div>
+    </div>
+    )
 }
