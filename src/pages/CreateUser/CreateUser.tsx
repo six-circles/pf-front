71b791db58e0994d836ca18485dc94c52a9832c7
michelle margan <michimargan@hotmail.com>
2023-05-13 10:57:08 -0300

    register user

--- src/pages/CreateUser/CreateUser.tsx
+++ src/pages/CreateUser/CreateUser.tsx
@@ -1,32 +1,91 @@
+import { useNavigate } from "react-router"
 import styles from "./CreateUser.module.scss"
+import {useState} from "react"
+// import axios from "axios"
+import { urlAxios } from "../../utils"
+import validation from "./validate"
+
+
 
 export default function (){
+    const navigate=useNavigate()
+    const handleNavigate=()=> navigate("/login")
+  
+    const [form,setForm]=useState({
+        name:"",
+        email:"",
+        phone:"",
+        birthday:"",
+        password:"",
+        repeatPassword:"",
+    })
+
+    const [error,setError]=useState({
+        password:"",
+       
+    })
+
+    const newUser:object={
+        name:form.name,
+        email:form.email,
+        phone:form.phone,
+        birthday:form.birthday,
+        password:form.password,
+    }
+
+
+    const handleChange=(e:any)=>{
+        const property:string= e.target.name
+        const value:string= e.target.value
+        setForm({...form,[property]:value})
+        setError({...validation({...form,[property]:value})}) 
+    }
+    
+    const submit= async()=>{
+         try {
+                await urlAxios.post("/user",newUser)
+                alert("Usuario creado con exito")
+                setForm({
+                name:"",
+                email:"",
+                phone:"",
+                birthday:"",
+                password:"",
+                repeatPassword:""
+                })
+            } 
+        catch (error:any) {
+                console.log(error.message)
+                alert("Las contraseñas deben coincidir")
+            }
+        
+    }
     return( 
     <div className={styles.contenedor}>
         <div className={styles.contenedor}>
-            <button className={styles.buttonAtras}>Atrás</button>
+            <button className={styles.buttonAtras} onClick={handleNavigate}>Atrás</button>
             <h2 className={styles.title}>Registrate</h2>
-            <form className={styles.contForm}>
+            <form className={styles.contForm} onSubmit={submit}>
                 <div className={styles.contForm}>
                     <div className={styles.form}>
                         <label htmlFor="Usuario">Usuario</label>
-                        <input type="text" name="usuario" className={styles.input}/>
+                        <input type="text" name="name" className={styles.input} value={form.name} onChange={handleChange}/>
 
                         <label htmlFor="Contraseña" className={styles.name}>Contraseña</label>
-                        <input type="password" name="contraseña" className={styles.input}/>
-
+                        <input type="password" name="password" className={styles.input} value={form.password} onChange={handleChange}/>
+                            <p>{error.password}</p>
                         <label htmlFor="Repita contraseña">Repita contraseña</label>
-                        <input type="password" name="repita contraseña" className={styles.input}/>
+                        <input type="password" name="repeatPassword" value={form.repeatPassword} className={styles.input}  onChange={handleChange}/>
                     </div>
                     <div className={styles.form}>
                         <label htmlFor="Correo">Correo</label>
-                        <input type="email" name="correo" className={styles.input}/>
+                        <input type="email" name="email" className={styles.input} value={form.email} onChange={handleChange}/>
 
                         <label htmlFor="Telefono">Telefono</label>
-                        <input type="number" name="telefono" className={styles.input}/>
+                        <input type="number" name="phone" className={styles.input} value={form.phone} onChange={handleChange}/>
 
                         <label htmlFor="Fecha de nacimiento">Fecha de nacimiento</label>
-                        <input type="date" name="fecha de nacimiento" className={styles.input}/>
+                        <input type="date" name="birthday" className={styles.input} value={form.birthday} onChange={handleChange}/>
                     </div>
                 </div>
                 <button className={styles.buttonRes}>Registrar</button>
