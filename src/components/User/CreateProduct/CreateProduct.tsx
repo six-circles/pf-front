a37f639805989a6f495b225d7e03e03fa2989735
NelsonAMR <nelson.macias99@gmail.com>
2023-05-15 07:38:12 -0600

    hot fix createProduct 3

--- src/components/User/CreateProduct/CreateProduct.tsx
+++ src/components/User/CreateProduct/CreateProduct.tsx
@@ -26,7 +26,6 @@ export default function CreateProduct() {
     const user: any = window.localStorage.getItem("user");
     let id;
     let config;
-    let obj;
 
     if (!user) {
       id = "";
@@ -38,16 +37,16 @@ export default function CreateProduct() {
       config = {
         headers: { _id: id.id },
       };
-
-      obj = {
-        title: form.title,
-        image: [form.image1, form.image2, form.image3],
-        stock: Number(form.stock),
-        price: Number(form.price),
-        description: form.description,
-        userId: id,
-      };
     }
+
+    const obj = {
+      title: form.title,
+      image: [form.image1, form.image2, form.image3],
+      stock: Number(form.stock),
+      price: Number(form.price),
+      description: form.description,
+      userId: id.id,
+    };
 
     try {
       await urlAxios.post("/product", obj, config);
