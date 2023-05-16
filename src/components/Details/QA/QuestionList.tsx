29e61ed20bdb4d85cd17b72329945f67a0079da1
NelsonAMR <nelson.macias99@gmail.com>
2023-05-16 08:14:10 -0600

    iconos en cartas, mejoras en estilos

--- src/components/Details/QA/QuestionList.tsx
+++ src/components/Details/QA/QuestionList.tsx
@@ -1,61 +1,64 @@
-import React, { useState } from 'react';
-import "./QA.scss"
+import React, { useState } from "react";
+import "./QA.scss";
 interface Question {
   id: number;
   text: string;
 }
 
-function QuestionList(props:any) {
+function QuestionList(props: any) {
   const [questions, setQuestions] = useState<Question[]>([]);
-  const [newQuestion, setNewQuestion] = useState('');
+  const [newQuestion, setNewQuestion] = useState("");
 
   const handleQuestionSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (newQuestion.trim() === '') {
+    if (newQuestion.trim() === "") {
       return;
     }
 
     const newQuestionObj: Question = {
       id: questions.length + 1,
-      text: newQuestion
+      text: newQuestion,
     };
 
     setQuestions([...questions, newQuestionObj]);
-    setNewQuestion('');
+    setNewQuestion("");
   };
-console.log(props.questions);
 
   return (
-    <div className='card'>
+    <div className="card">
       <h1>Preguntas</h1>
       <br />
       <div>
-       {props?.questions&& props?.questions.map((item:any,index:any)=>(
-        <div key={index}>
-          <p>{item.userName}</p>
-        <p >{item.body}</p>
-        </div>
-       ))}
+        {props?.questions &&
+          props?.questions.map((item: any, index: any) => (
+            <div key={index}>
+              <p>{item.userName}</p>
+              <p>{item.body}</p>
+            </div>
+          ))}
       </div>
-       <br />
+      <br />
       <form onSubmit={handleQuestionSubmit}>
-        <div className='new'>
-        <input className='new'
-          type="text"
-          value={newQuestion}
-          onChange={event => setNewQuestion(event.target.value)}
-          placeholder="Escribe tu pregunta"
-        />
+        <div className="new">
+          <input
+            className="new"
+            type="text"
+            value={newQuestion}
+            onChange={(event) => setNewQuestion(event.target.value)}
+            placeholder="Escribe tu pregunta"
+          />
         </div>
         <br />
-        <div className='send'>
-        <button  className='send' type="submit">Publicar pregunta</button>
-      </div>
+        <div className="send">
+          <button className="send" type="submit">
+            Publicar pregunta
+          </button>
+        </div>
       </form>
 
       <ul>
-        {questions.map(question => (
+        {questions.map((question) => (
           <li key={question.id}>{question.text}</li>
         ))}
       </ul>
