70a2031426207042250203d3f4c9db9293f22986
michelle margan <michimargan@hotmail.com>
2023-05-16 15:15:19 -0300

    cambios michi

--- src/pages/CreateUser/CreateUser.tsx
+++ src/pages/CreateUser/CreateUser.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 // import axios from "axios"
 import { urlAxios } from "../../utils";
 import validation from "./validate";
-import Swal from "sweetalert2";
 
 export default function () {
   const navigate = useNavigate();
@@ -22,7 +21,7 @@ export default function () {
   const [error, setError] = useState({
     password: "",
     email: "",
-    birthday: "",
+    birthday:"",
   });
 
   const newUser: object = {
@@ -34,55 +33,44 @@ export default function () {
   };
 
 
+  
 
 
-
-  const handleChange = (e: any) => {
-    e.preventDefault()
-    const property: string = e.target.name
-    const value: string = e.target.value
-    setForm({ ...form, [property]: value })
-    setError({ ...validation({ ...form, [property]: value }) })
-  }
-
-  const submit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      await urlAxios.post("/user", newUser)
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: "Usuario creado con exito",
-        showConfirmButton: false,
-        timer: 1500
-      });
-      setForm({
-        name: "",
-        email: "",
-        phone: "",
-        birthday: "",
-        password: "",
-        repeatPassword: ""
-      })
-      navigate("/login")
+    const handleChange=(e:any)=>{
+        e.preventDefault()
+        const property:string= e.target.name
+        const value:string= e.target.value
+        setForm({...form,[property]:value})
+        setError({...validation({...form,[property]:value})}) 
     }
-    catch (error: any) {
-      if (error.response.data.error.includes("duplicate key")) {
-        Swal.fire({
-          icon: "error",
-          title: "Email ya registrado",
-        });
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: error.response.data.error,
-        });
-      }
+    
+    const submit= async(event:React.FormEvent)=>{
+        event.preventDefault();
+         try {
+                await urlAxios.post("/user",newUser)
+                alert("Usuario creado con exito")
+                setForm({
+                    name:"",
+                    email:"",
+                    phone:"",
+                    birthday:"",
+                    password:"",
+                    repeatPassword:""
+                })
+                navigate("/login")
+            } 
+            catch (error:any) {
+                if (error.response.data.error.includes("duplicate key")){
+                    alert("Email ya registrado")
+                }else{
 
+                    alert(error.response.data.error)
+                }
+             
+            }
+        
     }
-
-  }
-  return (
+    return( 
     <div className={styles.contenedor}>
       <div className={styles.contenedor}>
         <button className={styles.buttonAtras} onClick={handleNavigate}>
@@ -149,10 +137,10 @@ export default function () {
                 value={form.birthday}
                 onChange={handleChange}
               />
-              <p className={styles.errorPasw}>{error.birthday}</p>
+              <p className={styles.errorBirth}>{error.birthday}</p>
             </div>
           </div>
-          {error.password.length === 0 && error.email.length === 0 ? (
+          {error.password.length === 0 && error.email.length === 0 && error.birthday.length ===0? (
             <button className={styles.buttonRes}>Registrar</button>
           ) : (
             <button className={styles.buttonResDisabl} disabled>
