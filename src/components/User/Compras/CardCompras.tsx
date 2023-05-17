5b36a4c794baba8ad5df51882ced7fdb0b004edc
NelsonAMR <nelson.macias99@gmail.com>
2023-05-17 17:12:35 -0600

    mejoras en estilos

--- src/components/User/Compras/CardCompras.tsx
+++ src/components/User/Compras/CardCompras.tsx
@@ -1,23 +1,22 @@
-import styles from "./CardCompras.module.scss"
+import styles from "./CardCompras.module.scss";
 
 interface Product {
-    id: string;
-    name: string;
-    image: string;
-    price: number;
-  }
-export default function (props:Product){
-    return(
-        <div className={styles.card}>
-                <div className={styles.card_image }>
-                    <img src={props.image[0]} alt={props.name.slice(0, 10)} />
-                </div>
-                <div className={styles.info}>
-                    <h3 className={styles.title}>{props.name}</h3>
-                    <p className={styles.precio}>${props.price}</p>
-                </div>
-                
-                <p className={styles.entrega}>Entregado el 08/02/2023</p>
-            </div>
-    )
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+}
+export default function (props: Product) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card_image}>
+        <img src={props.image[0]} alt={props.name.slice(0, 10)} />
+      </div>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{props.name}</h3>
+        <p className={styles.precio}>${props.price}</p>
+        <p className={styles.entrega}>Entregado el 08/02/2023</p>
+      </div>
+    </div>
+  );
 }
