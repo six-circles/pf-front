9f24a3497a47144628693a7a98b07c38e6254291
michelle margan <michimargan@hotmail.com>
2023-05-19 16:55:16 -0300

    links footer ingresar/salir, estilos y nuevo/usado en moreproduct

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -4,13 +4,33 @@ import { checkAuth} from "../../utils";
 import styles from "./User.module.scss";
 import { CgProfile } from "react-icons/cg";
 import { Link } from "react-router-dom";
-
+import Swal from "sweetalert2";
 function User() {
   const navigate: Function = useNavigate();
 
   useEffect(() => {
     checkAuth("product", navigate);
   }, []);
+
+  const handleClick = () => {
+
+    Swal.fire({
+      title: '¿Desea cerrar sesión?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, salir!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.localStorage.setItem("user", "");
+        navigate("/login");
+      }else{
+        navigate("/")
+      }
+    })
+  };
+
 
   return (
     <div className={styles.user}>
@@ -24,7 +44,7 @@ function User() {
           <Link to="/user/products">Ventas</Link>
           <Link to="/user/qa">Preguntas</Link>
           <Link to="/user/favoritos">Favoritos</Link>
-          <Link to="#">Salir</Link>
+          <Link to="login" onClick={handleClick} >Salir</Link>
         </div>
       </div>
     </div>
