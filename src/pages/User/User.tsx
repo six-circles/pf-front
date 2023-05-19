8a5483a025f1fea9d69a8bfe10112d2789b48828
NelsonAMR <nelson.macias99@gmail.com>
2023-05-19 13:50:43 -0600

    hotfix checkAutch

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { checkAuth} from "../../utils";
+import { checkAuth } from "../../utils";
 import styles from "./User.module.scss";
 import { CgProfile } from "react-icons/cg";
 import { Link } from "react-router-dom";
@@ -10,9 +10,9 @@ import { Link } from "react-router-dom";
 function User() {
   const navigate: Function = useNavigate();
 
-  useEffect(() => {
-    checkAuth("product", navigate);
-  }, []);
+  // useEffect(() => {
+  //   checkAuth("product", navigate);
+  // }, []);
 
   return (
     <div className={styles.user}>
@@ -21,7 +21,9 @@ function User() {
           <CgProfile />
         </h1>
         <div className={styles.perfil}>
-          <h3 className={styles.title}  onClick={() => navigate("/user")}>Mis Datos</h3>
+          <h3 className={styles.title} onClick={() => navigate("/user")}>
+            Mis Datos
+          </h3>
           <Link to="/user/shopping">Compras</Link>
           <Link to="/user/products">Ventas</Link>
           <Link to="/user/qa">Preguntas</Link>
