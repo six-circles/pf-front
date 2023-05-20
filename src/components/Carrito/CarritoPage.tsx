a2e72addc5678aa94b01e681b1309d59b8b6379a
michelle margan <michimargan@hotmail.com>
2023-05-20 14:11:51 -0300

    carrito delete

--- src/components/Carrito/CarritoPage.tsx
+++ src/components/Carrito/CarritoPage.tsx
@@ -1,23 +1,35 @@
 import styles from "./CarritoPage.module.scss";
 import { urlAxios } from "../../utils";
 import { getToken } from "../../utils/index";
+import { getCartProducts } from "../../redux/actions/carritoActions";
+import { useDispatch } from "react-redux";
 export default function (props: any) {
-  const user = getToken();
-  const token = user.token;
-  const prod = props.producto;
-  //"/:token/shoppingCart/:productID"
-  // const deleteProduct=async()=>{
-  //     await urlAxios(`${token}/shoppingCart/`)
-  // }
+    const dispatch:Function = useDispatch()
+    const user = getToken();
+    const token = user.token;
+    const prod = props.producto;
+    const id = props.producto._id
+ 
+
+  const deleteProduct=async()=>{
+     try {
+        await urlAxios.delete(`${token}/shoppingCart/${id}`)
+        alert("producto eliminado")
+        dispatch(getCartProducts())
+     } catch (error:any) {
+        console.log(error.message)
+     }
+  }
+  console.log(id)
   return (
     <div>
       {prod ? (
         <div className={styles.card}>
           <img src={prod.image[0]} alt={prod.title} className={styles.img} />
-          <p>{prod.price}</p>
           <p className={styles.title}>{prod.title}</p>
-          <p>{prod.puntuaction}</p>
-          <button className={styles.buttonEliminar}>X</button>
+          <p>${prod.price}</p>
+          {/* <p>{prod.puntuaction}</p> */}
+          <button className={styles.buttonEliminar} onClick={deleteProduct}>X</button>
         </div>
       ) : (
         <h3>No hay productos aún...</h3>
