6607645d94d8412e983d311cebf9961912c8c106
michelle margan <michimargan@hotmail.com>
2023-05-20 14:55:33 -0300

    boton details en carrito

--- src/components/Carrito/CarritoPage.tsx
+++ src/components/Carrito/CarritoPage.tsx
@@ -3,14 +3,14 @@ import { urlAxios } from "../../utils";
 import { getToken } from "../../utils/index";
 import { getCartProducts } from "../../redux/actions/carritoActions";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 export default function (props: any) {
     const dispatch:Function = useDispatch()
     const user = getToken();
     const token = user.token;
     const prod = props.producto;
     const id = props.producto._id
- 
-
+    const navigate= useNavigate()
   const deleteProduct=async()=>{
      try {
         await urlAxios.delete(`${token}/shoppingCart/${id}`)
@@ -20,16 +20,26 @@ export default function (props: any) {
         console.log(error.message)
      }
   }
-  console.log(id)
+  const details=(id:string)=>{
+    navigate(`/detail/${id}`)
+    }
+  const name = prod.title.slice(0,13)
   return (
     <div>
-      {prod ? (
+      { 
+        prod ? (
         <div className={styles.card}>
           <img src={prod.image[0]} alt={prod.title} className={styles.img} />
-          <p className={styles.title}>{prod.title}</p>
-          <p>${prod.price}</p>
+          <p className={styles.title}>{name}...</p>
+          <p className={styles.precio}>${prod.price}</p>
           {/* <p>{prod.puntuaction}</p> */}
+          <div className={styles.cantidad}>
+            <button>+</button>
+            <input type="number" className={styles.input}/>
+            <button>-</button>
+          </div>
           <button className={styles.buttonEliminar} onClick={deleteProduct}>X</button>
+          <button onClick={()=>details(id)}>Detalles</button>
         </div>
       ) : (
         <h3>No hay productos aún...</h3>
