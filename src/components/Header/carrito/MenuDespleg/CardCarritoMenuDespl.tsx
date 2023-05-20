f87aae67aaf3885278183e445870303d42471696
Michelle <118300108+MichiMargan@users.noreply.github.com>
2023-05-20 16:02:47 -0300

    Michi- carrito y menu despelgable (#63)
    * commit
    
    * changes
    
    * carrito delete
    
    * boton details en carrito
    
    * estilos carrito
    
    * menu desplegable

--- src/components/Header/carrito/MenuDespleg/CardCarritoMenuDespl.tsx
+++ src/components/Header/carrito/MenuDespleg/CardCarritoMenuDespl.tsx
@@ -1,18 +1,18 @@
 import styles from "./CardCarritoMenuDespl.module.scss"
 
 export default function(props:any){
-
-    const title = props.prueba.title.slice(0,5)
+    const p=props.product
+    const title = p.title.slice(0,5)
     
     return (
         <div>
 
           {props?  <div className={styles.card}>
-                <img src={props.prueba.image[0]} alt={props.prueba.image} className={styles.img}/>
-                <p className={styles.title}>{title}</p>
-                <p> ${props.prueba.price}</p>
-                <button className={styles.button}>+</button>
-                <button className={styles.button}>-</button>
+                <img src={p.image[0]} alt={p.image} className={styles.img}/>
+                <p className={styles.title}>{title}...</p>
+                <p> ${p.price}</p>
+                {/* <button className={styles.button}>+</button>
+                <button className={styles.button}>-</button> */}
             </div> : null}
         </div>
     )
