0b6fa83fc31a6ba6596a6ffc15c2100896c8e77c
Estefania Rizzo <tefa49395@gmail.com>
2023-05-20 23:39:28 -0500

    comit

--- src/components/chat/QuestionUser.tsx
+++ src/components/chat/QuestionUser.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./QA.scss";
 import { useParams } from "react-router-dom";
 import { getToken, urlAxios } from "../../utils";
@@ -9,61 +9,51 @@ interface Question {
 
 export function QuestionUser(props: any) {
   const [questions, setQuestions] = useState<Question[]>([]);
-  const [newQuestion, setNewQuestion] = useState("");
+  const [getQuestion, setGetQuestion] = useState("");
+
   const {id}=useParams()
 const {token, config}=getToken()
 
-  const handleQuestionSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-if (newQuestion.length > 0) {
-  const body ={token,productId:id,body:newQuestion}
-  console.log(body);
-  
-  try{
-    const response = await urlAxios.post( "/product/questions", body,config);
-    console.log(response);}
-  catch(error:any){
-    console.log(error.response.data);
-  }}}
-  
+useEffect(() => {
+handleQuestion();
+},[]);
+
+const handleQuestion = async () => {
+
+try{
+  const get = await urlAxios.get( `/questions/${token}`);
+  console.log(get);}
+catch(error:any){
+  console.log(error.response.data);
+}}
   return (
-    <Fragment>
-    <div className="card">
+    <Fragment> 
+    <div className="card">  
       <center><h1>Preguntas</h1></center>
       <br />
-      <div>
+      <div onSubmit={handleQuestion}>
         {props?.questions &&
           props?.questions.map((item: any, index: any) => (
             <div key={index}>
               <p>{item.userName}</p>
               <p>{item.body}</p>
+              
+              
             </div>
           ))}
       </div>
       <br />
-      <form onSubmit={handleQuestionSubmit}>
-        <div className="new">
-          <input
-            className="new"
-            type="text"
-            value={newQuestion}
-            onChange={(event) => setNewQuestion(event.target.value)}
-            placeholder="Escribe tu pregunta"
-          />
-        </div>
-        <br />
-        <center><div className="send">
-          <button className="send" type="submit">Publicar pregunta
-          </button>
-        </div></center>
-      </form>
+    
+     
 
       <ul>
-        {questions.map((question) => (
-          <li key={question.id}>{question.text}</li>
+        {questions.map((questions) => (
+          <li key={questions.id}>{questions.text}</li>
         ))}
       </ul>
       </div>
       </Fragment>
+     
+      
 );
 }
