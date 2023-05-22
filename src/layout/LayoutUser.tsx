8c58029db76320555066446fa46154554f18c4e4
michelle margan <michimargan@hotmail.com>
2023-05-22 00:12:10 -0300

    favoritos page

--- src/layout/LayoutUser.tsx
+++ src/layout/LayoutUser.tsx
@@ -4,9 +4,10 @@ import User from "../pages/User/User";
 import styles from "./LayoutUser.module.scss";
 import { checkAuth } from "../utils";
 
+
 function LayoutUser() {
   const navigate: Function = useNavigate();
-
+ 
   useEffect(() => {
     checkAuth("product", navigate);
   }, []);
--- src/pages/Favoritos/Favoritos.tsx
+++ src/pages/Favoritos/Favoritos.tsx
@@ -1,11 +1,25 @@
-import styles from "./Favoritos.module.scss"
+// import styles from "./Favoritos.module.scss"
+import { useSelector } from "react-redux"
+import { DetailProd } from "../../utils";
+// import { getFavorites } from "../../redux/actions/favoritosActions";
+// import { useDispatch } from "react-redux";
+import { Products } from "../../components";
+interface Products {
+  favoritos: DetailProd;
+}
+
+interface State {
+  favoritos: Products;
+}
 
 export default function (){
-
-   
+    // const dispatch=useDispatch()
+    const{favoritos}=useSelector((state:State)=>state.favoritos) 
 
     return (
-        <div>favoritos</div>
+        <div>
+            <Products products={favoritos}/>
+        </div>
 
     )
 
