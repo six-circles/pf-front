e49da1c44d401b566db0bf77b6d123dd2c76cb93
Estefania Rizzo <tefa49395@gmail.com>
2023-05-25 22:15:03 -0500

    comits style perfil

--- src/pages/User/QA/Preguntas.tsx
+++ src/pages/User/QA/Preguntas.tsx
@@ -3,6 +3,7 @@ import { Answers, QuestionUser, Questionmyuser } from "../../../components";
 import { Questions } from "../../../redux/actions/productActions.";
 import { useSelector } from "react-redux";
 
+
 export default function preguntas (){
     
    
