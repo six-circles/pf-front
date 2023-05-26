768d6ac0ead2dfffe808cca0a6ef9bb7f1db2d98
michelle margan <michimargan@hotmail.com>
2023-05-26 16:27:12 -0300

    cambio de ruta en more product

--- src/components/Details/MoreProd/MoreProduct.tsx
+++ src/components/Details/MoreProd/MoreProduct.tsx
@@ -1,10 +1,10 @@
 import { urlAxios } from "../../../utils";
 import { useEffect, useState } from "react";
 import CardProduct from "../../Home/Products/CardProduct";
-import styles from "./MoreProducts.module.scss"
-import { useSelector} from "react-redux";
+import styles from "./MoreProducts.module.scss";
+import { useSelector } from "react-redux";
 // import { useParams } from "react-router";
- import { DetailProd } from "../../../utils";
+import { DetailProd } from "../../../utils";
 
 interface Products {
   detail: DetailProd;
@@ -14,46 +14,40 @@ interface State {
   products: Products;
 }
 
-export default function MoreProduct(){
-  const [moreProducts,setMoreProducts]=useState([])
+export default function MoreProduct() {
+  const [moreProducts, setMoreProducts] = useState([]);
   const { detail } = useSelector((state: State) => state.products);
- 
-  const getID =()=>{
-    detail 
-    const {user} =detail
-    const idUser = user?._id
-    return idUser 
-  }
-  
-  const getProduct=async()=>{
-    const id = getID()
-    const {data}= await urlAxios(`/product/moreproducts/${id}`)
-    setMoreProducts(data)
-  }
-  useEffect(() => {
-    getProduct();
-}, []);
-  
-  
 
-    
-    const moreProd = moreProducts.slice(0,5)
-    
-    return (
+  const user = detail.user;
+  const email = user?.email;
+
+  const getProduct = async (email: string) => {
+    const { data } = await urlAxios(`/user?email=${email}`);
+    const { products } = data;
+    setMoreProducts(products);
+  };
+  useEffect(() => {
+    getProduct(email);
+  }, []);
+
+  const moreProd = moreProducts.slice(0, 5);
+
+  return (
     <div className={styles.contenedor}>
       <h1 className={styles.title}>Más productos del vendedor</h1>
       <div className={styles.card}>
         {moreProd.map((item: any) => (
-        <CardProduct
-          key={item._id}
-          id={item._id}
-          name={item.title}
-          image={item.image}
-          punctuation={item.punctuations}
-          price={item.price}
-          condition={item.condition}
-        />
-      ))}
+          <CardProduct
+            key={item._id}
+            id={item._id}
+            name={item.title}
+            image={item.image}
+            punctuation={item.punctuations}
+            price={item.price}
+            condition={item.condition}
+          />
+        ))}
       </div>
     </div>
-    )}
+  );
+}
