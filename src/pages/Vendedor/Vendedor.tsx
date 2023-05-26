c0b9ac5c3fc6600d5ed844f5b4c4edb4fe2d38ed
michelle margan <michimargan@hotmail.com>
2023-05-26 19:29:18 -0300

    perfil vendedor actualizado

--- src/pages/Vendedor/Vendedor.tsx
+++ src/pages/Vendedor/Vendedor.tsx
@@ -4,6 +4,10 @@ import { urlAxios } from "../../utils";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Products } from "../../components";
+import { Link } from "react-router-dom";
+import styles from "./Vendedor.module.scss";
+import { getProducts } from "../../redux/actions/productActions.";
+import { Rating } from "../../components";
 interface Products {
   detail: DetailProd;
 }
@@ -16,17 +20,29 @@ export default function () {
   const { detail } = useSelector((state: State) => state.products);
   const user = detail.user;
   const email = user?.email;
+  const name = user?.name;
+
   const [allProducts, setAllProducts] = useState([]);
+  const [puntuaction, setPunctuation] = useState(0);
+
   const getAllProd = async () => {
     const { data } = await urlAxios(`/user?email=${email}`);
     const products = data?.products;
     setAllProducts(products);
+    const puntuation = data.punctuation;
+    setPunctuation(puntuation);
   };
   useEffect(() => {
+    getProducts();
     getAllProd();
   }, []);
   return (
     <div>
+      <h1>{name}</h1>
+      <p>Cantidad de ventas: 100</p>
+      <p>
+        <Rating punctuation={puntuaction} />
+      </p>
       <div>
         {allProducts ? (
           <Products products={allProducts} />
