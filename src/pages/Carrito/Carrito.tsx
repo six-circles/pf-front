4d42c96776bb546a444c007265551b75e9eb56f6
NelsonAMR <nelson.macias99@gmail.com>
2023-05-26 20:32:43 -0600

    estilos en cards de carrito page

--- src/pages/Carrito/Carrito.tsx
+++ src/pages/Carrito/Carrito.tsx
@@ -1,40 +1,46 @@
 // import { CardCarritoMenuDespl } from "../../components"
-import { CarritoPage,Resumen } from "../../components/index"
-import { useSelector } from "react-redux"
-import styles from "./Carrito.module.scss"
-import { urlAxios } from "../../utils"
+import { CarritoPage, Resumen } from "../../components/index";
+import { useSelector } from "react-redux";
+import styles from "./Carrito.module.scss";
+import { urlAxios } from "../../utils";
 import { getToken } from "../../utils/index";
 
 export default function () {
-    const { cartProducts } = useSelector((state: any) => state.carrito)
-    const user = getToken();
-    const token = user.token;
+  const { cartProducts } = useSelector((state: any) => state.carrito);
+  const user = getToken();
+  const token = user.token;
 
-    const handleCheckout = async () => {
-        console.log("PRODUCTOS: ", cartProducts);
+  const handleCheckout = async () => {
+    console.log("PRODUCTOS: ", cartProducts);
 
-        const urlBack = import.meta.env.VITE_BACK_URL || '';
-        try {
-            const { data } = await urlAxios.post(`${urlBack}/mercadopago/${token}`, { shoppingCart: cartProducts });
-            console.log("ID mercadoPago: ", data.url);
-            window.location.href = data.url;
-        } catch (error) {
-            console.log("ERROR: ", error);
-        }
-
+    const urlBack = import.meta.env.VITE_BACK_URL || "";
+    try {
+      const { data } = await urlAxios.post(`${urlBack}/mercadopago/${token}`, {
+        shoppingCart: cartProducts,
+      });
+      console.log("ID mercadoPago: ", data.url);
+      window.location.href = data.url;
+    } catch (error) {
+      console.log("ERROR: ", error);
     }
-    
+  };
 
-    return (
-        <div className={styles.contCarrito}>
-
-            <div className={styles.card}>
-                {cartProducts.length ? cartProducts.map((p: any) => <CarritoPage key={p._id} producto={p} />) : <h3>No hay nada por aquí...</h3>}
-            </div>
-           {cartProducts.length? <div className={styles.resumen}>
-                <Resumen productos={cartProducts} key={cartProducts._id}/>
-                <button className={styles.buttonComprar} onClick={handleCheckout}>Comprar ahora</button>
-            </div> : null}
-        </div>
-    )
+  return (
+    <div className={styles.contCarrito}>
+      <div className={styles.card}>
+        {cartProducts.length ? (
+          cartProducts.map((p: any) => <CarritoPage key={p._id} producto={p} />)
+        ) : (
+          <h3>No hay nada por aquí...</h3>
+        )}
+      </div>
+      {cartProducts.length && (
+        <Resumen
+          productos={cartProducts}
+          key={cartProducts._id}
+          onClick={handleCheckout}
+        />
+      )}
+    </div>
+  );
 }
