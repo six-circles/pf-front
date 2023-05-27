fe3d9077bc9211621767c09aea41a1a21c5efbbb
michelle margan <michimargan@hotmail.com>
2023-05-27 20:26:44 -0300

    arreglo del estado

--- src/pages/Vendedor/Vendedor.tsx
+++ src/pages/Vendedor/Vendedor.tsx
@@ -1,16 +1,15 @@
-import { useSelector } from "react-redux";
 import { DetailProd } from "../../utils";
 import { urlAxios } from "../../utils";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Products } from "../../components";
-import { Link } from "react-router-dom";
 import styles from "./Vendedor.module.scss";
-import { getProductDetail } from "../../redux/actions/productActions.";
 import { Rating } from "../../components";
 import { useParams } from "react-router-dom";
+
 interface Products {
   detail: DetailProd;
+  products: any;
 }
 
 interface State {
@@ -18,36 +17,41 @@ interface State {
 }
 
 export default function () {
-  const { detail } = useSelector((state: State) => state.products);
-  const user = detail.user;
-  const email = user?.email;
-  const name = user?.name;
   const params = useParams();
   const id = params.id;
-  console.log(id);
-  console.log(detail);
+  const users = async () => {
+    const { data } = await urlAxios("/users");
+    const user = data.find((e: any) => id === e._id);
+    return user;
+  };
+
   const [allProducts, setAllProducts] = useState([]);
   const [puntuaction, setPunctuation] = useState(0);
+  const [name, setName] = useState("");
   useEffect(() => {
-    getProductDetail(id);
     getAllProd();
   }, []);
 
   const getAllProd = async () => {
+    const user = await users();
+    const email = user.email;
     const { data } = await urlAxios(`/user?email=${email}`);
     const products = data?.products;
     setAllProducts(products);
-    const puntuation = data.punctuation;
+    const puntuation = data?.punctuation;
     setPunctuation(puntuation);
+    const name = data?.name;
+    setName(name);
   };
+
   return (
     <div>
       <div className={styles.contenedor}>
         <h1 className={styles.title}>{name}</h1>
         <p className={styles.ventas}>Cantidad de ventas: 100</p>
-        <p className={styles.puntuacion}>
+        <div className={styles.puntuacion}>
           <Rating punctuation={puntuaction} />
-        </p>
+        </div>
       </div>
       <div>
         {allProducts ? (
