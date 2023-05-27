f86df0cde2e0aa5037b7263c6417f419ee1c858f
Estefania Rizzo <tefa49395@gmail.com>
2023-05-26 20:57:50 -0500

    link para caambiar de preguntas recibidas y mis preguntas con cursor default y verificacion de preguntas respondidas

--- src/components/chat/Answers.tsx
+++ src/components/chat/Answers.tsx
@@ -6,7 +6,6 @@ import { getToken, urlAxios } from "../../utils";
 interface Question {
   id: number;
   text: string;
-  productId: number; // Agrega la propiedad productId al tipo de datos Question
 }
 
 interface Answer {
@@ -23,7 +22,6 @@ export function Answers(props: any) {
   const { id } = useParams();
   const [getAnswer, setGetAnswer] = useState("");
   const [answered, setAnswered] = useState(false);
-  const [product, setProduct] = useState<any>(null); // Agrega el estado product
 
   const newAnswerObj = {
     body: newAnswer,
@@ -32,8 +30,7 @@ export function Answers(props: any) {
 
   useEffect(() => {
     handleAnswer();
-    checkAnswered();
-    fetchProduct(); // Obtener los detalles del producto al cargar la página
+    checkAnswered(); // Verificar si la pregunta ha sido respondida al cargar la página
   }, []);
 
   const handleAnswer = async () => {
@@ -59,7 +56,9 @@ export function Answers(props: any) {
       const response = await urlAxios.post("/product/questions/answers", newAnswerObj, config);
 
       setNewAnswer("");
-      setAnswered(true);
+      setAnswered(true); // Marcar la pregunta como respondida
+
+      // Guardar la pregunta respondida en el almacenamiento local
       localStorage.setItem(`answered_${props.id}`, JSON.stringify(newAnswerObj));
 
       handleAnswer();
@@ -69,6 +68,7 @@ export function Answers(props: any) {
   };
 
   const checkAnswered = () => {
+    // Verificar si la pregunta ha sido respondida anteriormente
     const answeredQuestion = localStorage.getItem(`answered_${props.id}`);
 
     if (answeredQuestion) {
@@ -76,24 +76,8 @@ export function Answers(props: any) {
     }
   };
 
-  const fetchProduct = async () => {
-    try {
-      const response = await urlAxios.get(`/product/${props.productId}`);
-      setProduct(response.data);
-    } catch (error: any) {
-      console.log(error.response.data);
-    }
-  };
-
   return (
     <Fragment>
-      {product && (
-        <div className={Style.productInfo}>
-          <h3>Producto: {product.name}</h3>
-          <p>Descripción: {product.description}</p>
-        </div>
-      )}
-
       {!answered && (
         <form onSubmit={handleAnswerSubmit}>
           <div className={Style.new}>
@@ -123,3 +107,4 @@ export function Answers(props: any) {
     </Fragment>
   );
 }
+
