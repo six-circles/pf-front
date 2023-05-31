adf24f44bb1e43898509763698ea8ea912ffa5aa
Ricardo Huallpayunca <rickhufer@outlook.com>
2023-05-31 11:59:47 -0500

    feat: mis ventas

---
+++ src/components/User/MisVentas/CardMisVentas.tsx
@@ -0,0 +1,48 @@
+import styles from "./CardMisVentas.module.scss";
+import { clearProducts } from "../../../redux/actions/productActions.";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+
+interface Product {
+  key: any;
+  id: any;
+  name: string;
+  image: any;
+  price: number;
+  punctuation: number;
+  cantidadCarrito: number;
+}
+export default function (props: Product) {
+  const navigate = useNavigate();
+  const dispatch: Function = useDispatch();
+  const id = props.id;
+  const handleClick = () => {
+    dispatch(clearProducts());
+    navigate(`/detail/${id}`);
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.card_image}>
+        <img
+          src={props.image[0].url}
+          alt={props.name.slice(0, 10)}
+          onClick={() => handleClick()}
+        />
+      </div>
+      <div className={styles.contTitle}>
+        <h4 className={styles.title} onClick={() => handleClick()}>
+          {props.name}... ({props.cantidadCarrito})
+        </h4>
+      </div>
+
+      <div className={styles.precio}>
+        {/* <p className={styles.precio}> */}
+        Total ${props.price * props.cantidadCarrito}
+        {/* </p> */}
+      </div>
+
+    </div>
+  );
+}
--- src/components/User/MisVentas/MisVentas.tsx
+++ src/components/User/MisVentas/MisVentas.tsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { getToken } from "../../../utils";
 import styles from "./MisVentas.module.scss";
+import { urlAxios } from "../../../utils";
+import CardMisVentas from "./CardMisVentas";
 
-function MisVentas() {
-  return <div>MisVentas</div>;
+export default function () {
+  const [compras, setCompras] = useState([]);
+  const { token } = getToken();
+
+  const getProducts = async () => {
+    const { data } = await urlAxios.get(`/sales/${token}`);
+    console.log(data);
+
+    const ordenes = data?.orders;
+    setCompras(ordenes);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1 className={styles.title}>MIS VENTAS</h1>
+      {compras?.length ? (
+        <div className={styles.cards}>
+          {compras.map((order: any) => (
+            <div className={styles.ordenes} key={Math.random()}>
+              <p>{order.created.slice(0, 10)} | <b>Comprador: <a href={`mailto:${order.userComprador.email}`}>{order.userComprador.email}</a></b></p>
+              {order.shoppingCart.map((product: any) => (
+                <CardMisVentas
+                  key={Math.random()}
+                  id={product._id}
+                  name={product.title}
+                  image={product.image}
+                  price={product.price}
+                  punctuation={product.punctuation}
+                  cantidadCarrito={product.cantidadCarrito}
+                />
+              ))}
+            </div>
+          ))}
+
+        </div>
+      ) : (
+        <h3>no realizaste compras aún...</h3>
+      )}
+    </div>
+  );
 }
-
-export default MisVentas;
